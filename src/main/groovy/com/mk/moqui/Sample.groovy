8adeb6b242a4ea00746f07544692df8cb4792337
andrewglevy <andrewglevy@gmail.com>
2018-08-29 16:47:09 -0700

    set up service structure

--- src/main/groovy/com/mk/moqui/Sample.groovy
+++ src/main/groovy/com/mk/moqui/Sample.groovy
@@ -6,6 +6,9 @@ import org.moqui.context.ExecutionContext
 @CompileStatic
 class Sample {
 	static Map<String, Object> logger(ExecutionContext ec) {
-		println("This is the sample class")
+		println('Custom Java class')
+		println(ec.context)
+		println(ec.getContext())
+		println(ec.getClass().getMethods())
 	}
 }
